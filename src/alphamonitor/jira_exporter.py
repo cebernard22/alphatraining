89d0861b4353a64ce0b43291f9941d539e1d8d38
cebernard22 <ce.bernard.perso@gmail.com>
2021-07-12 00:02:06 +0200

    Add files to create a python package "alphamonitor"

---
+++ src/alphamonitor/jira_exporter.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+"""basic python file to test the creation of a python module
+   The only purpose is to increase a number by one from TravisCI
+   At a latter stage this module will interact with Jira and retrieve how many open issues we have
+"""
+
+def add_one(number):
+    return number + 1
