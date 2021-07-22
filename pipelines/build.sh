4ab329b4bb0e88e64d74766ff2f81cebe84f7bad
cebernard22 <ce.bernard.perso@gmail.com>
2021-07-22 18:52:27 +0200

    adding commit option to... commit!

--- pipelines/build.sh
+++ pipelines/build.sh
@@ -6,5 +6,5 @@ echo "old path: ${PATH}"
 export PATH=/var/jenkins_home/.local/bin:${PATH}
 echo "new path: ${PATH}"
 #bump2version --verbose --no-commit --current-version ${currentVersion} patch setup.py
-bump2version --verbose --current-version ${currentVersion} patch setup.py
+bump2version --verbose --commit --current-version ${currentVersion} patch setup.py
 echo "version update completed"
