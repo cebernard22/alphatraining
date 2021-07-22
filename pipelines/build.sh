58c02fb27e5875f47893ef43b7576146a2def982
cebernard22 <ce.bernard.perso@gmail.com>
2021-07-22 18:37:46 +0200

    minor update in bash module

--- pipelines/build.sh
+++ pipelines/build.sh
@@ -6,3 +6,4 @@ echo "old path: ${PATH}"
 export PATH=/var/jenkins_home/.local/bin:${PATH}
 echo "new path: ${PATH}"
 bump2version --verbose --dry-run --current-version ${currentVersion} patch setup.py
+echo "version update completed"
