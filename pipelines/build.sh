79536124242d0ebe6143113365ebeb735579fbfa
cebernard22 <ce.bernard.perso@gmail.com>
2021-07-23 10:34:19 +0200

    moving commit step in another jenkins pipeline stage

--- pipelines/build.sh
+++ pipelines/build.sh
@@ -5,6 +5,5 @@ echo "increasing version from ${currentVersion}"
 echo "old path: ${PATH}"
 export PATH=/var/jenkins_home/.local/bin:${PATH}
 echo "new path: ${PATH}"
-#bump2version --verbose --no-commit --current-version ${currentVersion} patch setup.py
-bump2version --verbose --commit --current-version ${currentVersion} patch setup.py
+bump2version --verbose --current-version ${currentVersion} patch setup.py
 echo "version update completed"
